e47ec090c36928551e561fec212d87c5d7763a81
Frank Condezo <frankcondezo@gmail.com>
2023-11-27 13:12:55 -0500

    feat: refactor place foods (#4)

--- app/models/food_place.rb
+++ app/models/food_place.rb
@@ -1,2 +1,9 @@
 class FoodPlace < ApplicationRecord
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :phone_number, presence: true, length: {minimum:9, maximum:9}
+
+  def whatsapp_link
+    "https://wa.me/51#{phone_number}?text=Quiero pedir ..."
+  end
 end
