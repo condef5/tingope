f42415173ace75f5d95a1cf907c563d7341bf9c5
Frank Condezo <frankcondezo@gmail.com>
2023-11-26 21:21:20 -0500

    add validations

--- app/models/food_place.rb
+++ app/models/food_place.rb
@@ -1,5 +1,9 @@
 class FoodPlace < ApplicationRecord
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :phone_number, presence: true, length: {minimum:9, maximum:9}
+
   def whatsapp_link
-    "https://wa.me/#{phone_number}?text=Quiero pedir ..."
+    "https://wa.me/51#{phone_number}?text=Quiero pedir ..."
   end
 end
